20e6dd6cdece6a8a5d6c2d45be3066cc5fd40af9
lyauy <thibaut.yauy@gmail.com>
2021-02-11 18:09:47 +0100

    first commit

---
+++ src/components/crypto.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "crypto"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/tabcrypto.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "tabcrypto"
+}
+</script>
+
+<style scoped>
+
+</style>
